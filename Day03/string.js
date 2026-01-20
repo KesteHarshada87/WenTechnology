30931f34aa6d92bef8a5f95d3a5e05a94ec77eb0
Harshada Keste <kesteharshada87@gmail.com>
2026-01-20 22:47:57 +0530

    datatype,Backticks and string

---
+++ Day03/string.js
@@ -0,0 +1,16 @@
+let str1 = "Harshada";
+console.log(str1);
+console.log(typeof(str1));
+
+//string interpolation--` = backtick
+console.log(`Hello my name is: ${str1}`);
+
+let fname = "Rob";
+console.log(`fname is: ${fname}`);
+
+const gameCount = new String("Harshada");//---new is used for creating a object
+console.log(gameCount.length);
+console.log(gameCount.toUpperCase());
+console.log(gameCount.toLowerCase());
+console.log(gameCount.charAt(2));
+console.log(gameCount.indexOf('s'));
